8a6d44cdaeac1247161fec104c15ea35e882edbd
Aria Edmonds <aria@ar1as.space>
2022-12-28 20:58:29 +0800

    add sharkpad config

---
+++ keyboards/sharkpad/config.h
@@ -0,0 +1,46 @@
+// Copyright 2022 Aria Edmonds (@ar1a)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define RGB_DI_PIN GP26
+#define RGB_MATRIX_LED_COUNT 3
+//TODO: vial is using some older code that still uses DRIVER_LED_TOTAL
+// so we set it here, but once it's merged upstream we can remove the below
+#define DRIVER_LED_TOTAL 3
+#define RGBLED_NUM 3
+
+#define WS2812_PIO_USE_PIO1
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
+/*
+ * Enable specific rgb effects
+ */
+#define ENABLE_RGB_MATRIX_BAND_SAT
+#define ENABLE_RGB_MATRIX_BAND_VAL
+#define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_RAINDROPS
+#define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_BREATHING
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#define ENABLE_RGB_MATRIX_SPLASH
+#define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ keyboards/sharkpad/keymaps/default/keymap.c
@@ -0,0 +1,23 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ Z │ X │ C │ MU│
+     * ├───┼───┼───┼───┘
+     * │   │   │   │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT(KC_Z, KC_X, KC_C, KC_MUTE, KC_ESCAPE, KC_F2, KC_GRAVE)};
+
+led_config_t g_led_config = {{// Key Matrix to LED Index
+                              {1, 2, 3, NO_LED},
+                              {NO_LED, NO_LED, NO_LED, NO_LED}},
+                             {// LED Index to Physical Position
+                              {0, 32},
+                              {112, 32},
+                              {224, 32}},
+                             {// LED Index to Flag
+                              4, 4, 4}};
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {[0] = {ENCODER_CCW_CW(KC_MS_WH_DOWN, KC_MS_WH_UP)}};
